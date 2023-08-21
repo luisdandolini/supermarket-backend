5bf532d5935d6520e297548e3053958f20dd63d2
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-08-21 18:39:24 -0300

    feat: create method get and post

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+async function connectDatabase() {
+  await mongoose.connect('mongodb://localhost:27017')
+}
+
+const listItemSchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+  checked: Boolean,
+});
+
+const ListItem = mongoose.model('list_item', listItemSchema);
+
+app.get('/', (req, res) => {
+  res.send("Oi")
+});
+
+app.get('/list-items', async (req, res) => {
+  const items = await ListItem.find();
+  return res.json(items);
+});
+
+app.post('/list-items', async (req, res) => {
+  const newItem = await ListItem.create({
+    name: req.body.name,
+    quantity: req.body.quantity,
+    checked: req.body.checked,
+  });
+
+  return res.json(newItem);
+});
+
+app.listen(PORT, () => {
+  connectDatabase().catch((error) => {
+    console.log(`Erro connecting database: ${error}`);
+  });
+  console.log(`Example app listening on port ${PORT}`);
+});
